6c666d124eba4fc6e1c34a3cab4be6c772343029
wcp <972904383@qq.com>
2023-06-27 01:48:20 +0800

    rocket mq demo

--- src/main/java/vip/aquan/rocketmqdemo/RocketmqdemoApplication.java
+++ src/main/java/vip/aquan/rocketmqdemo/RocketmqdemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Rocketmq简单demo，链接：https://blog.csdn.net/QQ_hoverer/article/details/120115732
+ * 下载Rocketmq安装包， 启动mqnamesrv.cmd、启动mqbroker.cmd
  * 普通消息、顺序消息、事务消息、广播消息
  */
 @SpringBootApplication
